7478da9b9cb0be8f2d46740af800c9b2d5b6e928
Thibault Pelletier <thibault.pelletier@kitware.com>
2025-09-03 18:00:46 +0200

    ENH: Slicer 4.11 retro compatibility
    - Specific adaptations for Slicer 4.11 build

--- LayerDM/MRMLDM/vtkMRMLLayerDisplayableManager.h
+++ LayerDM/MRMLDM/vtkMRMLLayerDisplayableManager.h
@@ -52,7 +52,7 @@ protected:
   void UpdateFromMRML() override;
 
   int GetMouseCursor() override;
-  void SetHasFocus(bool hasFocus, vtkMRMLInteractionEventData* eventData) override;
+  void SetHasFocus(bool hasFocus) override;
 
 private:
   vtkSmartPointer<vtkMRMLLayerDMPipelineManager> m_pipelineManager;
