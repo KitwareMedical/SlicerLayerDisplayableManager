4927a407960e2ecb33f18fcb9344d7f65cea7fea
Thibault Pelletier <thibault.pelletier@kitware.com>
2025-09-08 10:43:46 +0200

    ENH: Slicer lib adaptation
    - Convert the MRMLDM to vtk.module
    - Remove the vtkSlicerLayerDMModuleMRMLDisplayableManagerExport file (auto generated by vtk module)
    - Add flag for VTK module auto init

--- LayerDM/MRMLDM/vtkMRMLLayerDMCameraSynchronizer.h
+++ LayerDM/MRMLDM/vtkMRMLLayerDMCameraSynchronizer.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "vtkSlicerLayerDMModuleMRMLDisplayableManagerExport.h"
+#include "vtkSlicerLayerDMModuleMRMLDisplayableManagerModule.h"
 
 // VTK includes
 #include <vtkObject.h>
