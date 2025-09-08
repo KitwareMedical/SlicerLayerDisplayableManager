ac21f04d34d4a27680b18d5d6a1500bff722284f
Thibault Pelletier <thibault.pelletier@kitware.com>
2025-09-08 10:43:46 +0200

    ENH: Slicer lib adaptation

--- LayerDM/MRMLDM/vtkMRMLLayerDMPipelineScriptedCreator.h
+++ LayerDM/MRMLDM/vtkMRMLLayerDMPipelineScriptedCreator.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "vtkSlicerLayerDMModuleMRMLDisplayableManagerExport.h"
+#include "vtkSlicerLayerDMModuleMRMLDisplayableManagerModule.h"
 
 #include "vtkMRMLLayerDMPipelineCallbackCreator.h"
 
--- LayerDM/MRMLDM/vtkMRMLLayerDMPythonUtil.h
+++ LayerDM/MRMLDM/vtkMRMLLayerDMPythonUtil.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "vtkSlicerLayerDMModuleMRMLDisplayableManagerExport.h"
+#include "vtkSlicerLayerDMModuleMRMLDisplayableManagerModule.h"
 
 // VTK includes
 #include <vtkObject.h>
