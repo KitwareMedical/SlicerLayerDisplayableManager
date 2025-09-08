3d62acad60132d7f99472cfc714ce4f8262e2010
Thibault Pelletier <thibault.pelletier@kitware.com>
2025-09-08 10:43:46 +0200

    ENH: Slicer lib adaptation

--- LayerDM/MRMLDM/vtkMRMLLayerDMObjectEventObserverScripted.h
+++ LayerDM/MRMLDM/vtkMRMLLayerDMObjectEventObserverScripted.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "vtkSlicerLayerDMModuleMRMLDisplayableManagerExport.h"
+#include "vtkSlicerLayerDMModuleMRMLDisplayableManagerModule.h"
 
 #include "vtkMRMLLayerDMObjectEventObserver.h"
 
