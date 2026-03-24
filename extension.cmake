a564e4e709a7921982f58af5f41895de5968acd7
Thibault Pelletier <thibault.pelletier@kitware.com>
2026-03-24 11:47:24 +0100

    fix: fix extension semantic tag
    In Slicer CI, the git repository is not initialized and querying the tag
    version fails with error code 128.
    
    fatal: not a git repository (or any of the parent directories): .git
    
    This fix makes semantic release update the CMake variable value directly.

--- extension.cmake
+++ extension.cmake
@@ -13,26 +13,10 @@ find_package(Slicer REQUIRED)
 include(${Slicer_USE_FILE})
 
 #-----------------------------------------------------------------------------
-# Extension version
-find_program(GIT_EXECUTABLE NAMES git REQUIRED)
-execute_process(
-  COMMAND "${GIT_EXECUTABLE}" "describe" "--tags" "--abbrev=0" "--match" "v[0-9]*"
-  RESULT_VARIABLE git_code
-  OUTPUT_VARIABLE git_stdout
-  ERROR_VARIABLE git_stderr
-)
+# Extension version (auto updated by semantic version)
+set(LayerDM_VERSION "v0.0.0")
 
-if(NOT git_code EQUAL 0)
-  message(FATAL_ERROR "Failed to determined project version using Git.\n"
-    "GIT_EXECUTABLE: ${GIT_EXECUTABLE}\n"
-    "Return Code: ${git_code}\n"
-    "Standard error:\n${git_stdout}\n"
-    "Standard error:\n${git_stderr}\n"
-  )
-endif()
-
-string(STRIP "${git_stdout}" git_stdout) # git output will end with a newline
-string(SUBSTRING "${git_stdout}" 1 -1 LayerDM_VERSION) # remove first character
+string(SUBSTRING "${LayerDM_VERSION}" 1 -1 LayerDM_VERSION) # remove first character
 string(REPLACE "." ";" version_components "${LayerDM_VERSION}") # this creates a list
 list(GET version_components 0 LayerDM_VERSION_MAJOR)
 list(GET version_components 1 LayerDM_VERSION_MINOR)
